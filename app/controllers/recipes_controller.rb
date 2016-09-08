eee848cb33a3b26a6b2d0a6f788db6da4dbf5ce6
Nathaniel Giron <nathaniel.giron@gmail.com>
2016-09-08 03:16:32 +0000

    create partials

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -24,6 +24,20 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
   
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = "Your recipe was updated!"
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
+  end
+  
   private
     def recipe_params
       params.require(:recipe).permit(:name, :summary, :description)
