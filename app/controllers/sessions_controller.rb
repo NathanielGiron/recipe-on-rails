c51f82c39a9bf21a0a1193d3a4fadebb001bfd6d
Nathaniel Giron <nathaniel.giron@gmail.com>
2016-09-15 08:23:39 +0000

    routes, login form, create/destroy sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  
+  def new
+  end
+  
+  def create
+    chef = Chef.find_by(email: params[:session][:email].downcase)
+    if chef && chef.authenticate(params[:session][:password])
+      session[:chef_id] = chef.id
+      flash[:success] = "You have successfully logged in!"
+      redirect_to chef_path(chef)
+    else
+      flash[:error] = "Incorrect Username or Password."
+      render 'new'
+    end
+  end
+  
+  def destroy
+    session[:chef_id] = nil
+    flash[:success] = "You have logged out."
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,10 @@ Rails.application.routes.draw do
   resources :chefs , except: [:new]
   get 'signup', to: 'chefs#new'
   
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
