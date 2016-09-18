6bff051d48efa45ace76b3c9281c11d391a1c3a4
Nathaniel Giron <nathaniel.giron@gmail.com>
2016-09-18 02:49:03 +0000

    helper methods, before actions for chefs controller

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,4 +1,6 @@
 class ChefsController < ApplicationController
+  before_action :set_chef, only: [:show, :edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
   
   def index
     @chefs = Chef.paginate(page: params[:page], per_page: 3).order(created_at: :desc)
@@ -19,16 +21,13 @@ class ChefsController < ApplicationController
   end
   
   def show
-    @chef = Chef.find(params[:id])
     @recipes = @chef.recipes.paginate(page: params[:page], per_page: 3).order(created_at: :desc)
   end
   
   def edit
-    @chef = Chef.find(params[:id])
   end
   
   def update
-    @chef = Chef.find(params[:id])
     if @chef.update(chef_params)
       flash[:success] = "Your profile has been updated!"
       redirect_to recipes_path
@@ -40,5 +39,16 @@ class ChefsController < ApplicationController
   private
     def chef_params
       params.require(:chef).permit(:chefname, :email, :password)
+    end
+    
+    def set_chef
+      @chef = Chef.find(params[:id])
+    end
+    
+    def require_same_user
+      if current_user != @chef
+        flash[:error] = "You can only edit your own profile"
+        redirect_to root_path
+      end
     end
 end
